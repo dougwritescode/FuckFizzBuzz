371efc99611d03abc473ba0a8d8a6fd866ded397
Doug Walter <dougwritescode@gmail.com>
2023-10-08 00:08:46 -0600

    added main function calls for demo purposes

--- FizzBuzzes/OneLine/OneLineFizzBuzz.py
+++ FizzBuzzes/OneLine/OneLineFizzBuzz.py
@@ -1,2 +1,5 @@
 # FizzBuzz in one line
 fizz_buzz_one_line = lambda N = 15: print('\n'.join([f'{i} FizzBuzz' if i % 3 == 0 and i % 5 == 0 else f'{i} Fizz' if i % 3 == 0 else f'{i} Buzz' if i % 5 == 0 else f'{i}' for i in range(1, N + 1)]))
+
+if __name__ == "__main__":
+    fizz_buzz_one_line()
--- FizzBuzzes/OneLineGeneral/OneLineGeneral.py
+++ FizzBuzzes/OneLineGeneral/OneLineGeneral.py
@@ -1,2 +1,5 @@
 # FizzBuzz generalized in one line
 general_fizz_buzz_one_line = lambda N = 15, d = {3: 'Fizz', 5: 'Buzz'}: print('\n'.join([f"{i} {''.join(d[k] for k in filter(lambda a: i % a == 0, d.keys()))}" for i in range(1, N + 1)]))
+
+if __name__ == "__main__":
+    general_fizz_buzz_one_line()
