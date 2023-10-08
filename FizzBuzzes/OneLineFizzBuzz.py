76f6f6c47b78b8bfe92e894a44ac9cbc01440e7c
Doug Walter <dougwritescode@gmail.com>
2023-10-07 23:20:49 -0600

    fizzbuzz in one line

---
+++ FizzBuzzes/OneLineFizzBuzz.py
@@ -0,0 +1,2 @@
+# FizzBuzz in one line
+fizz_buzz_one_line = lambda N = 15: print('\n'.join([f'{i} FizzBuzz' if i % 3 == 0 and i % 5 == 0 else f'{i} Fizz' if i % 3 == 0 else f'{i} Buzz' if i % 5 == 0 else f'{i}' for i in range(1, N + 1)]))
