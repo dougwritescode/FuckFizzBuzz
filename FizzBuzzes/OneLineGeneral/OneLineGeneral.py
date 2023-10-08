db6ff33a2f9b17cde4d1bf8a12d4646bd85e4177
Doug Walter <dougwritescode@gmail.com>
2023-10-07 23:45:55 -0600

    Added general one line implementation, folder

---
+++ FizzBuzzes/OneLineGeneral/OneLineGeneral.py
@@ -0,0 +1,2 @@
+# FizzBuzz generalized in one line
+general_fizz_buzz_one_line = lambda N = 15, d = {3: 'Fizz', 5: 'Buzz'}: print('\n'.join([f"{i} {''.join(d[k] for k in filter(lambda a: i % a == 0, d.keys()))}" for i in range(1, N + 1)]))
