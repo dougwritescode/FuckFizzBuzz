256c61173f1f44460e1c7849839dfbc6c4dbdeb9
Doug Walter <dougwritescode@gmail.com>
2023-10-08 15:21:45 -0600

    Basic version and test

---
+++ FizzBuzzes/Basic/Basic.py
@@ -0,0 +1,15 @@
+
+def FizzBuzz():
+    for i in range(1, 16):
+        if i % 5 == 0 and i % 3 == 0:
+            print(f'{i} FizzBuzz')
+        elif i % 5 == 0:
+            print(f'{i} Buzz')
+        elif i % 3 == 0:
+            print(f'{i} Fizz')
+        else:
+            print(i)
+
+
+if __name__ == "__main__":
+    FizzBuzz()
