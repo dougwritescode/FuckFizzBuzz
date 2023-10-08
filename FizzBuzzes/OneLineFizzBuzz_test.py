ec02f840e7c91bac4aa63c660b6a90e2be4cc915
Doug Walter <dougwritescode@gmail.com>
2023-10-07 23:27:32 -0600

    test for one line fb framed

---
+++ FizzBuzzes/OneLineFizzBuzz_test.py
@@ -0,0 +1,15 @@
+import io
+from contextlib import redirect_stdout
+from unittest import TestCase
+
+from OneLineFizzBuzz import fizz_buzz_one_line
+
+
+class FizzBuzzOneLineTest(TestCase):
+
+    def test_fb_one_line(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
