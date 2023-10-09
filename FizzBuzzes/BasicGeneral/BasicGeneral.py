9d98fd503c31eba8844792980cf33ff2729e24e6
Doug Walter <dougwritescode@gmail.com>
2023-10-08 21:02:10 -0600

    type-controlled dict input

--- FizzBuzzes/BasicGeneral/BasicGeneral.py
+++ FizzBuzzes/BasicGeneral/BasicGeneral.py
@@ -1,6 +1,6 @@
 
 # Generalized basic FizzBuzz
-def fizz_buzz_general(n: int = 15, d: dict = {3: 'Fizz', 5: 'Buzz'}):
+def fizz_buzz_general(n: int = 15, d: dict[int, str] = {3: 'Fizz', 5: 'Buzz'}):
     for i in range(1, n + 1):
         wordlist = [d[k] for k in filter(lambda a: i % a == 0, d.keys())]
         print(f"{i} {''.join(wordlist)}")
