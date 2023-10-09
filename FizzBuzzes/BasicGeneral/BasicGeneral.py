9537e5109347d2c969a1baf5b46c5ea8fc334840
Doug Walter <dougwritescode@gmail.com>
2023-10-08 21:01:37 -0600

    renamed params to d for concision

--- FizzBuzzes/BasicGeneral/BasicGeneral.py
+++ FizzBuzzes/BasicGeneral/BasicGeneral.py
@@ -1,8 +1,8 @@
 
 # Generalized basic FizzBuzz
-def fizz_buzz_general(n: int = 15, params: dict = {3: 'Fizz', 5: 'Buzz'}):
+def fizz_buzz_general(n: int = 15, d: dict = {3: 'Fizz', 5: 'Buzz'}):
     for i in range(1, n + 1):
-        wordlist = [params[k] for k in filter(lambda a: i % a == 0, params.keys())]
+        wordlist = [d[k] for k in filter(lambda a: i % a == 0, d.keys())]
         print(f"{i} {''.join(wordlist)}")
 
 
